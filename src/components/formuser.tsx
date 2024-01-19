dbe7320b7336d17e243e498ba87857b23c986744
caio.stuart <caio.stuart@gruppe.com.br>
2024-01-19 12:52:42 -0300

    Adicionando token nas requisições.

--- src/components/formuser.tsx
+++ src/components/formuser.tsx
@@ -1,19 +1,42 @@
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import "../styles/components/formuser.css"
 import { handleInput } from "../functions/inputs/handleInput";
+import api from "../config";
+import { useNavigate } from "react-router-dom";
 
 export default function FormUser(){
-    const [inputsValues, setInputsValues] = useState<IFormUser>();
+    const [inputsValues, setInputsValues] = useState<IFormUser>({
+        name: "",
+        email: "",
+        password: ""
+    });
+    const navigate = useNavigate();
+
+    function register(e: FormEvent){
+        e.preventDefault();
+    
+        api
+        .post("/register", inputsValues)
+        .then((response) => {
+            localStorage.setItem("token", response.data.token);
+            navigate("/");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
 
     return (
         <div id="formUser">
             <div className="formUser__logo"></div>
-            <form className="formUser__form">
-                <h1></h1>
-                <input type="text" placeholder="Nome" name="name" value={inputsValues?.name} onChange={(e) => handleInput(setInputsValues, e)} />
-                <input type="text" placeholder="E-mail" name="email" value={inputsValues?.email} onChange={(e) => handleInput(setInputsValues, e)} />
-                <input type="text" placeholder="Senha" name="password" value={inputsValues?.password} onChange={(e) => handleInput(setInputsValues, e)} />
-                <button type="button"></button>
+            <form className="formUser__form" onSubmit={register}>
+                <h1 className="formUser___title">Registre-se</h1>
+                <div className="formUser___inputs">
+                    <input type="text" placeholder="Nome" name="name" value={inputsValues?.name} onChange={(e) => handleInput(setInputsValues, e)} />
+                    <input type="text" placeholder="E-mail" name="email" value={inputsValues?.email} onChange={(e) => handleInput(setInputsValues, e)} />
+                    <input type="text" placeholder="Senha" name="password" value={inputsValues?.password} onChange={(e) => handleInput(setInputsValues, e)} />
+                </div>
+                <button type="submit" className="formUser___button">Salvar</button>
             </form>
         </div>
     )
--- src/config.tsx
+++ src/config.tsx
@@ -2,10 +2,31 @@ import axios from "axios";
 const baseUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 const api = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        token: localStorage.token
+    baseURL: baseUrl
+});
+
+api.interceptors.request.use((request) => {
+    const token = localStorage.getItem("token");
+
+    if(token){
+        request.headers.Authorization = `Bearer ${token}`;
     }
+
+    return request;
+});
+
+api.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if(error.response.status === 401){
+        window.location.href = "/page401";
+    }
+
+    if(error.reponse.status === 403){
+        window.location.href = "/page404";
+    }
+
+    return Promise.reject(error);
 });
 
 export default api;
